b5fdfa624f6c1f954995a845c002bdd597452c49
Marios Zindilis <marios@zindilis.com>
2011-10-04 10:38:13 +0300

    Added variable.py

---
+++ variable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: variable.py
+ 
+i = 5
+print i
+i = i + 1
+print i
+ 
+s = '''Αυτή η συμβολοσειρά καταλαμβάνει πολλαπλές γραμμές.
+Αυτή είναι η δεύτερη γραμμή.'''
+print s
