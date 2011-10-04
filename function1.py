2939f5112fca37d3796d226e6f45a718cd074540
Marios Zindilis <marios@zindilis.com>
2011-10-04 21:14:55 +0300

    Started function1.py.

---
+++ function1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: function1.py
+ 
+def sayHello():
+  print 'Γειά σου, κόσμε!' # Η πλοκάδα που αποτελεί τη συνάρτηση
+# Τέλος της συνάρτησης
+ 
+sayHello() # Κλήση της συνάρτησης
