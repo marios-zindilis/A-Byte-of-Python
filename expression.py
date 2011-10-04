4df71fcb15f555576f740801f445535385d57866
Marios Zindilis <marios@zindilis.com>
2011-10-04 10:50:18 +0300

    Started expression.py

---
+++ expression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: expression.py
+ 
+length = 5
+breadth = 2
+ 
+area = length * breadth
+print 'Το εμβαδό είναι', area
+print 'Η περίμετρος είναι', 2 * (length + breadth)
