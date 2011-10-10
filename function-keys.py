80ec87564e3b61390bdf2720269b79e324285a2f
Marios Zindilis <marios@zindilis.com>
2011-10-10 11:28:32 +0300

    Started function-keys.py.

---
+++ function-keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: function-keys.py
+
+def func(a, b=5, c=10):
+  print 'Το a είναι:', a, ', το b είναι:', b, ', και το c είναι:', c
+ 
+func(3, 7)
+func(25, c=24)
+func(c=50, a=100)
