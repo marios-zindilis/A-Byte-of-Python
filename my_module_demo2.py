48e9392d6fda83bb1714506634ed0310cf2f5c84
Marios Zindilis <marios@zindilis.com>
2011-10-18 19:59:00 +0300

    Started my_module_demo2.py.

---
+++ my_module_demo2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: my_module_demo2.py
+ 
+from my_module import sayhi, version
+# ή αλλιώς:
+# from mymodule import *
+ 
+sayhi()
+print 'Έκδοση:', version
